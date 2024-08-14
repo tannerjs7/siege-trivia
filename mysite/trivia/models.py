7a6de4b938223ab0dac24dcd99e551039c5bf7d7
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-08-13 21:12:12 -0400

    Add suggestion model

--- mysite/trivia/models.py
+++ mysite/trivia/models.py
@@ -4,7 +4,17 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=500)
     answer_text = models.CharField(max_length=100)
-    pub_date = models.DateTimeField('date published')
+    published_date = models.DateTimeField('date published')
+    suggested = models.BooleanField(name='suggested', default=False)
 
     def __str__(self):
         return self.question_text
+    
+
+class Suggestion(models.Model):
+    suggestion_text = models.CharField(max_length=500)
+    answer_text = models.CharField(max_length=100)
+    suggested_date = models.DateTimeField('date suggested')
+
+    def __str__(self):
+        return self.suggestion_text
