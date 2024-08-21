e03353105034f80bad818d11d8314b9b0659c084
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-08-21 19:50:23 -0400

    Add views
    Added detail and suggest views. Modified index view.

--- mysite/trivia/urls.py
+++ mysite/trivia/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('suggest/', views.suggest, name='suggest'),
 ]
--- mysite/trivia/views.py
+++ mysite/trivia/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+import random
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    random_q = random.choice(Question.objects.all())
+    return HttpResponse(random_q)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def suggest(request):
+    return HttpResponse("Suggest a question.")
