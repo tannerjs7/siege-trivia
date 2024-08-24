66404546f5ea94f6bbb10d1306736ef4dbf12231
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-08-23 21:03:53 -0400

    t Create index templates
    Created templates folder, created index template,
    changed index view to render the template

--- mysite/trivia/views.py
+++ mysite/trivia/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 import random
 
 
 def index(request):
     random_q = random.choice(Question.objects.all())
-    return HttpResponse(random_q)
+    context = {'random_q': random_q}
+    return render(request, 'trivia/index.html', context)
 
 
 def detail(request, question_id):
