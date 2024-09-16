9ec7866464d0453a02b7fb22c28c2abe4b7d9633
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-09-16 19:46:42 -0400

    Create suggestion template, modify index template
    Also updated urls and views.

--- mysite/trivia/urls.py
+++ mysite/trivia/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'trivia'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('detail/', views.detail, name='detail'),
     path('suggest/', views.suggest, name='suggest'),
 ]
--- mysite/trivia/views.py
+++ mysite/trivia/views.py
@@ -1,19 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from django.urls import reverse
+from .models import Question, Suggestion
+from datetime import datetime
 import random
 
+# Add 404 exceptions
 
 def index(request):
-    random_q = random.choice(Question.objects.all())
+    # random_q = random.choice(Question.objects.all())
+    random_q = list(Question.objects.all())
+    random.shuffle(random_q)
     context = {'random_q': random_q}
     return render(request, 'trivia/index.html', context)
 
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(request):
+    all_suggestions = list(Suggestion.objects.all())
+    context = {'all_suggestions': all_suggestions}
+    return render(request, 'trivia/detail.html', context)
 
 
 def suggest(request):
-    return HttpResponse("Suggest a question.")
+    try:
+        suggestion = Suggestion(
+            suggestion_text=request.POST['question'],
+            answer_text=request.POST['answer'],
+            suggested_date=datetime.now()
+        )
+    except (KeyError, Suggestion.DoesNotExist):
+        return render(request, 'trivia/detail.html', {
+            'error_message': 'Try again.'
+        })
+    else:
+        suggestion.save()
+        return HttpResponseRedirect(reverse('trivia:detail'))
