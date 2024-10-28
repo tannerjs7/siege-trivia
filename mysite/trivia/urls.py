3eb26fbd4203cc99b9d26734a43bc0e2288b86c5
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-10-28 14:46:34 -0400

    Add styles to the index page
    Added background gradient, centered text, and added borders.

--- mysite/trivia/urls.py
+++ mysite/trivia/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'trivia'
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('<list:exclude_ids>/<str:exclude_id>/', views.index, name='index'),
     path('detail/', views.detail, name='detail'),
     path('suggest/', views.suggest, name='suggest'),
 ]
